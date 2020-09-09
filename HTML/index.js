231ebf5288663bf5df3d2826d0af06d7175236c9
TarlanG <qtarlan@yahoo.com>
2020-09-09 17:00:57 -0400

    changes

---
+++ HTML/index.js
@@ -0,0 +1,17 @@
+console.log("Welcome");
+
+// var element = document.getElementById('root');
+// console.log(element);
+
+// const fs = require('fs');
+
+// fs.readFile('HTML/note.js', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// })
+
+// function foo(){
+//   console.log(this);
+// };
+
+// foo();
