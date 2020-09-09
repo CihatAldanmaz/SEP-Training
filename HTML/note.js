a49b18e52b94f311bdaf21d7252b0df67c201989
hongye <zhouhye@gmail.com>
2020-09-09 16:08:59 -0400

    0909classnote

---
+++ HTML/note.js
@@ -0,0 +1,19 @@
+// HTTP request: GET| POST| DELETE | PUT| PATCH (CRUD)
+// PUT: update teh entire object
+//PATCH: update some field (more efficient)
+//could we send data to server by 'get'? Yes  
+// i18n: internationalization; l10n:localization
+//meta
+//SEO
+//ECMASCcript(e.g. ES5, ES6) vs JavaScript vs NodeJs 
+console.log('Welcome')
+var element = document.getElementById('root');
+console.log(element.id); 
+
+//function foo(){
+//    console.log(this);
+//}
+//foo();
+// what is the differenc between blcok element and in-line element
+//
+
