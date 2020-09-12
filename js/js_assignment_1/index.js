76facea114cf4b7960350dd7aa7c03762b44a5eb
Treagan Birbal <treaganbirbal@pursuit.org>
2020-09-12 17:12:57 -0400

    implementation of isPalindrome function

--- js/js_assignment_1/index.js
+++ js/js_assignment_1/index.js
@@ -3,13 +3,19 @@
 // Input: x = 32243
 //Output: 34223
 
-const numbReverse = (num) => {
-  let newStr = "";
-  const strNum = num.toString();
-  for (let i = strNum.length - 1; i >= 0; i--) {
-    newStr += strNum[i];
-  }
-  return Number(newStr);
+// const numbReverse = (num) => {
+//   let newStr = "";
+//   const strNum = num.toString();
+//   for (let i = strNum.length - 1; i >= 0; i--) {
+//     newStr += strNum[i];
+//   }
+//   return Number(newStr);
+// };
+
+// 2. Write a JavaScript function that checks whether a passed string is palindrome or not? A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g.,madam or nurses run
+
+const isPalindrome = (str) => {
+  return str.toLowerCase() === str.split("").reverse().join("");
 };
 
-console.log(numbReverse(32243));
+console.log(isPalindrome("racecar"));
