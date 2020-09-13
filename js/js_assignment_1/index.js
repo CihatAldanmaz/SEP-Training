d86db3a6c4ca983698a19edef9d44750aeae66be
Treagan Birbal <treaganbirbal@pursuit.org>
2020-09-13 12:39:59 -0400

    implementation of problem 24-bubbleSort

--- js/js_assignment_1/index.js
+++ js/js_assignment_1/index.js
@@ -291,3 +291,28 @@ const firstNonRepeatingChar = (str) => {
     }
   }
 };
+
+//24. Write a JavaScript function to apply Bubble Sort algorithm.
+
+function bubbleSort(arr) {
+  let noSwap;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    noSwap = true;
+    for (let j = 0; j <= i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        noSwap = false;
+        swap(arr, j, j + 1);
+      }
+    }
+    if (noSwap) break;
+  }
+  return arr;
+}
+
+function swap(arr, index1, index2) {
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+  return arr;
+}
+
