018d66136a439bf8d46a1db3d8f61b54bb94584e
Treagan Birbal <treaganbirbal@pursuit.org>
2020-09-13 12:41:47 -0400

    fix: problem 24-sort descending order

--- js/js_assignment_1/index.js
+++ js/js_assignment_1/index.js
@@ -299,7 +299,7 @@ function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     noSwap = true;
     for (let j = 0; j <= i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+      if (arr[j] < arr[j + 1]) {
         noSwap = false;
         swap(arr, j, j + 1);
       }
