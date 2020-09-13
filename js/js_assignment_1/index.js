ebd01872c1b4e9eb9c0407192e3d6c035c0924ca
Treagan Birbal <treaganbirbal@pursuit.org>
2020-09-13 11:53:48 -0400

    chore: implementation of problem 18-binarySearch

--- js/js_assignment_1/index.js
+++ js/js_assignment_1/index.js
@@ -218,3 +218,24 @@ function charCount(str) {
   }
   return charMap;
 }
+
+//18. Write a function for searching JavaScript arrays with a binary search.
+
+const binarySearch = (arr, val) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  while (arr[middle] !== val && left <= right) {
+    if (val < arr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+    middle = Math.floor((left + right) / 2);
+  }
+  if (arr[middle] === val) {
+    return middle;
+  }
+  return -1;
+};
