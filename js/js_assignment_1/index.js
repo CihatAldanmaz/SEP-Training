1d47061b8e31b5c05d8948774002ba413374f94d
Treagan Birbal <treaganbirbal@pursuit.org>
2020-09-13 14:31:55 -0400

    implementation of problem 26-longest substring of non-repeating chars

--- js/js_assignment_1/index.js
+++ js/js_assignment_1/index.js
@@ -331,3 +331,29 @@ const findLongestName = (strArr) => {
   }
   return longestWord;
 };
+
+//26. Write a JavaScript function to find longest substring in a given a string without repeatingcharacters
+
+function longestSubstring(str) {
+  let longest = 0;
+  let longestStr = "";
+  let seen = {};
+  let start = 0;
+  let next = 0;
+
+  while (next < str.length) {
+    let char = str[next];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    if (longest < next - start + 1) {
+      longest = next - start + 1;
+      longestStr = str.slice(start, next + 1);
+    }
+    seen[char] = next + 1;
+    next++;
+  }
+  return longestStr;
+}
+
+console.log(longestSubstring("ababcdefba"));
