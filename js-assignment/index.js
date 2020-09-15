cec59a741998feb9000b229b20e7fd2a34b06542
hmshreyas7 <hmshreyas7@gmail.com>
2020-09-15 04:00:58 -0700

    Add function to get first non-repeating character

--- js-assignment/index.js
+++ js-assignment/index.js
@@ -218,6 +218,26 @@ getLetterCountInString = (str, letter) => {
 }
 // console.log(getLetterCountInString("hello world", "o"));
 
+// 23. Get first non-repeating character.
+getFirstNonRepeat = (str) => {
+    let charCount = new Map();
+
+    for (let i = 0; i < str.length; i++) {
+        if (charCount.has(str[i])) {
+            charCount.set(str[i], charCount.get(str[i]) + 1);
+        } else {
+            charCount.set(str[i], 1);
+        }
+    }
+
+    for (key of charCount.keys()) {
+        if (charCount.get(key) === 1) {
+            return key;
+        }
+    }
+}
+// console.log(getFirstNonRepeat("abaceddbc"));
+
 // 24. Apply bubble sort (descending).
 bubbleSort = (arr) => {
     let n = arr.length;
