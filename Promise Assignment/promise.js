0b2ac585299a03c7a3016e95e118bb4fbce8c21e
Steve <chenkailee9@gmail.com>
2020-09-16 22:13:32 -0700

    update fetch  practice

--- Promise Assignment/promise.js
+++ Promise Assignment/promise.js
@@ -1,48 +1,82 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then((response) => response.json())
-  .then((json) => {
-    console.log('=== fetch Get ====');
-    console.log(json);
-  });
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log('=== fetch Get ====');
+//     console.log(json);
+//   });
+
+function myfetch_get(url) {
+    return new Promise((res, rej) => {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                // Typical action to be performed when the document is ready:
+                const response = {
+                    json: function () {
+                        return JSON.parse(xhttp.responseText);
+                    },
+                };
+                res(response);
+            }
+        };
+        xhttp.open('GET', url, true);
+        xhttp.send();
+    });
+}
+
+myfetch_get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+
 fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => {
+        console.log('=== fetch Post ====');
+        console.log(json);
+    });
+
+function myfetch_post(url, object) {
+    return new Promise((res, rej) => {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                // Typical action to be performed when the document is ready:
+                const res_arr = [...xhttp.responseText];
+
+                console.log(res_arr.filter((obj)=> obj["userId"]=== object['userId']));
+                const response = {
+                    json: function () {
+                        return JSON.parse(xhttp.responseText);
+                    },
+                };
+                res(response);
+                
+            }
+        };
+        xhttp.open('GET', url, true);
+        xhttp.send();
+    });
+}
+const object = {
     title: 'foo',
     body: 'bar',
     userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => {
-    console.log('=== fetch Post ====');
-    console.log(json);
-  });
-
-function myfetch(url) {
-  return new Promise((res, rej) => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        // Typical action to be performed when the document is ready:
-        const response = {
-          json: function () {
-            return JSON.parse(xhttp.responseText);
-          },
-        };
-        res(response);
-      }
-    };
-    xhttp.open('GET', url, true);
-    xhttp.send();
-  });
 }
 
-myfetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+myfetch_post('https://jsonplaceholder.typicode.com/posts', object)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 
 // fetch('https://jsonplaceholder.typicode.com/todos/1')
 //   .then((response) => response.json())
