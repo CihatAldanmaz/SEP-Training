b02496e57e72fe34338406e65fbed89999e6a6c1
Geethika Rajanala <geethu@Geethikas-Air.attlocal.net>
2020-09-17 12:42:22 -0400

    fetch, promise implementation

--- JS-notes/index.js
+++ JS-notes/index.js
@@ -68,7 +68,7 @@
 // foo(obj);
 // console.log(obj);// {name:"jane"}
 
-//            var   |  let    |  const 
+//            var   |  let    |  const
 // scope: function     block    block
 // hoist: true          no       no
 
@@ -87,9 +87,8 @@
 // }
 // foo();
 
-
 // function foo(){
-    
+
 //     console.log(a);// undefined
 //     if(true){
 //         var  a = 5;
@@ -155,9 +154,6 @@
 // const p2 = new Person('Jane',18);
 // p2.walk();
 
-
-
-
 // function foo(){
 //     "use strict";
 //     console.log(x);
@@ -167,7 +163,6 @@
 
 // foo();
 // console.log(x);
-
 
 // OOP
 
@@ -225,7 +220,6 @@
 // Employee.prototype = Object.create(Person.prototype);
 // Employee.prototype.constructor = Employee;
 
-
 // const p = new Person('patrick',18);
 // console.log(p.name);
 // p.name = 'New Name';
@@ -241,14 +235,12 @@
 // const arr  = [1,2,3];
 // console.log(sum(arr));
 
-
 // function sum(arr){
 //     return arr.reduce((acc,cur)=>{
 //         console.log(acc , cur)
 //     },0)
 // }
-// map, filter, reduce 
-
+// map, filter, reduce
 
 // console.log(arr.forEach((value, index , array)=>{
 //     console.log(value,index,array)
@@ -291,7 +283,6 @@
 //     return item * 3;
 // }))
 
-
 // const obj = {name:'patrick'};
 // var name = 'name';
 // console.log(obj.name, obj[name]);
@@ -326,7 +317,6 @@
 //     console.log(key,value)
 // })
 
-
 /// name, patrick
 /// age, 18
 
@@ -353,17 +343,416 @@
 // shadow copy
 // const a = {name:'patrick'};
 // const b = a;
+
+// deepy copy
 // const c = {...a};
-const d = {name:{value:'patrick'}};
-const e = {...d}
+// const d = {name:{value:'patrick'}};
+// const e = {...d}
 // b.name = 'newName';
 // console.log(a === b);
 // console.log(a,b);
 // console.log(c === a);
 // c.name = 'newName';
 // console.log(a,c);
-e.name.value = 'newName';
-console.log(e,d)
-console.log([]);
+// e.name.value = 'newName';
+// console.log(e,d)
+// console.log([]);
 // Employeee => Person : employee.getSalary
 // Array => Object : array.forEach
+
+// destructure
+// const obj  = {name:'patrick',age:18};
+// const {name,age,company} = obj;
+// const name = obj["name"];
+// const age = obj["age"];
+// const company = obj["company"]
+// console.log(name,age,company);
+
+// const arr = [{name:'patrick'},()=>{}]
+// const [state,useState] = arr;
+// const state = arr[0];
+// const useState = arr[1];
+// console.log(state,useState);
+
+// arrow function
+// const foo = function(){
+//     console.log('foo')
+//     console.log(this);
+// }.bind(this);
+
+// const foo2 = ()=>{
+//     console.log('foo2');
+//     console.log(this);
+// }
+
+// const obj = {
+//     foo:function(){
+//         console.log(this);
+//     }.bind(this),
+//     foo2:()=>{
+//         console.log(this);
+//     }
+// }
+// obj.foo();
+// obj.foo2();
+
+// closure
+// curying function
+// function add(...num){
+//     //>>>> closure
+//     return function(...num2){
+//         //>>>> closure
+//         return function(...num3){
+//             //console.log(num3);
+//             //num.concat(num2).concat(num3).reduce((acc, cur)=>acc+cur, 0);
+//             return [...num,...num2,...num3].reduce((acc, cur)=>acc+cur, 0);
+//         }
+//     }
+// }
+// const add = (...num)=>(...num2)=>(...num3)=>[...num,...num2,...num3].reduce((acc, cur)=>acc+cur, 0);
+
+// console.log(add(5,5,5)()(7,2,1));// output 25;
+// console.log(add()(2,5)(7,2,1));// output 17;
+
+// function foo(a,b,c){
+// }
+
+// foo(1,2,3,4,5,)
+
+// function foo(){
+//     ///>>>>
+//     console.log('hello');
+//     var a =5;
+//     ///>>>>
+//     return function(){
+//         console.log(a);
+//     }
+// }
+
+// let a =foo();
+// a();
+// console.log(a);
+
+// IIFE
+
+// (function foo(){
+//     console.log('hello')
+// })()
+
+// JS module pattern
+// const Controller = (function(){
+//     var a = 5;
+
+//     return {init:()=>{
+//         console.log('hello' + a);
+//     }}
+// })()
+// console.log(Controller)
+// Controller.init();
+
+// 0 2 4 3 1
+// Event loop: Call Stack | Async APIS | Task Queue
+// console.log(0);
+// setTimeout(function(){
+//     console.log('1')
+// },1000)
+// console.log(2);
+// setTimeout(function(){
+//     console.log('3')
+// },0)
+// console.log(4);
+
+// function foo(){
+//     for(var i = 0; i<5;i++){
+//         // 5 let i
+//         (function(){
+//             var num = i;
+//             setTimeout(()=>{
+//                 console.log(num);
+//             },num * 1000)
+//         })()
+//     }
+
+//     // var i = 0;
+//     // let a = i * 1000;
+//     // function foo(){
+//     //     console.log(i); // 0;
+
+//     // }
+//     // setTimeout(foo,a)
+//     // i=5;
+
+// }
+
+// foo();
+
+// async |
+
+// function foo(){
+//     console.log('init');
+// }
+
+// function foo2(){
+//     console.log('foo2');
+// }
+
+// function randomTimer (){
+//     return Math.random()*10000;
+// }
+
+// function callCbWithTimer(callbackFn){
+//     let timer = randomTimer()
+//     console.log('delay timer:',timer);
+//     setTimeout(callbackFn,timer);
+// }
+
+// function callFooWithTimer(){
+//     setTimeout(foo,randomTimer());
+// }
+// function callFoo2WithTimer(){
+//     setTimeout(foo2,randomTimer());
+// }
+
+// callCbWithTimer(()=>{
+//     callCbWithTimer(()=>{
+//         callCbWithTimer(foo2);
+//     })
+// })
+
+// let data;
+
+function getUserDataPromise(userId) {
+    return new Promise((resolve, reject) => {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          // Typical action to be performed when the document is ready:
+          data = JSON.parse(xhttp.responseText);
+          resolve(data);
+        }
+      };
+      xhttp.open(
+        'GET',
+        'https://jsonplaceholder.typicode.com/todos/' + userId,
+        true
+      );
+      xhttp.send();
+    });
+  }
+  
+  // getUserDataPromise(1)
+  //   .then((data1) => {
+  //     console.log('data1');
+  //     logMessage(data1);
+  //     return getUserDataPromise(2);
+  //   })
+  //   .then((data2) => {
+  //     console.log('data2');
+  //     logMessage(data2);
+  //     return getUserDataPromise(3);
+  //     // return 'hello';
+  //   })
+  //   .then((data3) => {
+  //     console.log('data3');
+  //     logMessage(data3);
+  //   });
+  
+  // function getUserData(userId, callbackFn) {
+  //   var xhttp = new XMLHttpRequest();
+  //   xhttp.onreadystatechange = function () {
+  //     if (this.readyState == 4 && this.status == 200) {
+  //       // Typical action to be performed when the document is ready:
+  //       data = JSON.parse(xhttp.responseText);
+  //       callbackFn(data);
+  //     }
+  //   };
+  //   xhttp.open(
+  //     'GET',
+  //     'https://jsonplaceholder.typicode.com/todos/' + userId,
+  //     true
+  //   );
+  //   xhttp.send();
+  // }
+  // getUserData(2,logMessage);
+  // get userData 1 => log it => get userData 2 => log it = > getUerData3 => log it;
+  
+  // getUserData(1, (data1) => {
+  //   logMessage(data1),
+  //     getUserData(2, (data2) => {
+  //       logMessage(data2);
+  //       getUserData(3, (data3) => {
+  //         logMessage(data3);
+  //       });
+  //     });
+  // });
+  
+  // function logMessage(msg) {
+  //   console.log(msg);
+  // }
+  
+  function randomTimer() {
+    return Math.random() * 5000;
+  }
+  
+  // function foo() {
+  //   let timer = randomTimer();
+  //   console.log('delayTimer:', timer);
+  //   setTimeout(() => {
+  //     logMessage('hello');
+  //   }, timer);
+  // }
+  
+  // foo();
+  
+  // JS queue
+  // [] push to enqueue, shift to dequeue;
+  // [] unshift to enqueue, pop to dequeue;
+  
+  // this : bind | apply | call
+  class MyPromise {
+    constructor(executorFn) {
+      this.promiseState = 'pending';
+      this.resolve = function (data) {
+        setTimeout(() => {
+          this.promiseState = 'fulfilled';
+          this.currentData = data;
+          console.log('resolve');
+          while (this.thenCallbackQueue.length > 0) {
+            const curThenCallbackFn = this.thenCallbackQueue.shift();
+            if (this.currentData instanceof MyPromise) {
+              console.log('this is a promise');
+              this.currentData.then((thenData) => {
+                console.log('test');
+                this.currentData = curThenCallbackFn(thenData);
+              });
+            } else {
+              console.log('test');
+              this.currentData = curThenCallbackFn(this.currentData);
+            }
+          }
+        }, 0);
+      };
+      this.reject = function () {};
+      this.thenCallbackQueue = [];
+      executorFn(this.resolve.bind(this), this.reject.bind(this));
+    }
+  
+    then(thenCallbackFn) {
+      this.thenCallbackQueue.push(thenCallbackFn);
+      return this;
+    }
+  }
+  
+  const p = new MyPromise((resolve, reject) => {
+    let timer = randomTimer();
+    console.log('delayTimer:', timer);
+    setTimeout(() => {
+      resolve('hello');
+    }, timer);
+  })
+    .then((data1) => {
+      console.log('data1', data1);
+      return new MyPromise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('Hey Patrick');
+        }, 1000);
+      });
+    })
+    .then((data2) => {
+      console.log('data2', data2);
+    });
+  const p2 = new Promise((resolve, reject) => {
+    let timer = randomTimer();
+    console.log('delayTimer:', timer);
+    setTimeout(() => {
+      resolve('hello');
+    }, timer);
+  })
+    .then((data1) => {
+      console.log('data1', data1);
+      console.log(p2);
+      return 'data2 hello';
+    })
+    .then((data2) => {
+      console.log(data2);
+    });
+  
+  // const p = new MyPromise((res, rej) => {
+  //   res('hello');
+  // });
+  
+  // p.then((data) => {
+  //   console.log(data);
+  // });
+  
+  // console.log(0);
+  
+  // const p = new Promise((resolve, reject) => {
+  //   resolve(1);
+  // }).then((data) => {
+  //   console.log(data);
+  // });
+  
+  // console.log(2);
+  
+  // fetch('https://jsonplaceholder.typicode.com/todos/1')
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     console.log('=== fetch Get ====');
+  //     console.log(json);
+  //   });
+  // fetch('https://jsonplaceholder.typicode.com/posts', {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 1,
+  //   }),
+  //   headers: {
+  //     'Content-type': 'application/json; charset=UTF-8',
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     console.log('=== fetch Post ====');
+  //     console.log(json);
+  //   });
+  
+  // function myfetch(url) {
+  //   return new Promise((res, rej) => {
+  //     var xhttp = new XMLHttpRequest();
+  //     xhttp.onreadystatechange = function () {
+  //       if (this.readyState == 4 && this.status == 200) {
+  //         // Typical action to be performed when the document is ready:
+  //         const response = {
+  //           json: function () {
+  //             return JSON.parse(xhttp.responseText);
+  //           },
+  //         };
+  //         res(response);
+  //       }
+  //     };
+  //     xhttp.open('GET', url, true);
+  //     xhttp.send();
+  //   });
+  // }
+  
+  // myfetch('https://jsonplaceholder.typicode.com/todos/1')
+  //   .then((response) => response.json())
+  //   .then((json) => console.log(json));
+  
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  fetch('https://jsonplaceholder.typicode.com/todos/2')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  fetch('https://jsonplaceholder.typicode.com/todos/3')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  
+  function logUsers(user1, user2, user3) {
+    console.log(user1, user2, user3);
+  }
+  
+  // Promise catch, Promise.all, fetch
---
+++ JS/imp.js
@@ -0,0 +1,110 @@
+class MyPromise {
+
+    constructor(executor) {
+        this.state = 'pending';
+        this.data = null;
+        try {
+            executor(this.resolve.bind(this), this.reject.bind(this));
+        } catch (error) {
+            this.reject(error);
+        }
+    }
+    resolve(data) {
+        if (this.state === 'pending') {
+            this.state = 'fulfilled';
+            this.data = data;
+        }
+    }
+    reject(data) {
+        if (this.state === 'pending') {
+            this.data = data;
+            this.state = 'rejected';
+        }
+    }
+    then(forFulfilled, forRejected) {
+        if (typeof forFulfilled != "function") {
+            forFulfilled = () => { };
+        }
+        if (typeof forRejected != "function") {
+            forRejected = () => { };
+        }
+        if (this.state === 'fulfilled') {
+            try {
+                forFulfilled(this.data);
+                return this;
+            } catch (error) {
+                forRejected(error);
+            }
+        }
+
+        if (this.state === 'rejected') {
+            try {
+                forRejected(this.data);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        return new MyPromise((resolve, reject) => {
+
+        })
+    }
+    catch(forRejected) {
+        return this.then(undefined, forRejected);
+    }
+
+}
+
+
+new MyPromise((resolve, reject) => {
+    resolve();
+    // reject();
+    // throw 'on-no';
+}).then(
+    value => {
+        console.log('a');
+    }
+)
+    .catch(
+        value => {
+            console.log('error');
+        }
+    )
+
+    // (
+    //     err => (console.log('catchininginging')));
+
+// new Promise((resolve, reject) => {
+//     //resolve(null);
+//     // reject();
+//     throw 'on-no';
+// }).then(
+//     value => {
+//         console.log('成功处理');
+//     }
+// ).catch(err => console.log('catchininginging'));
+
+
+function myfetch(url, data) {
+  return new Promise((res, rej) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const response = {
+          json: function () {
+            return JSON.parse(xhttp.responseText);
+          },
+        };
+        res(response);
+      }
+    };
+    xhttp.open('GET', url, true);
+    xhttp.send();
+  });
+}
+
+myfetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
