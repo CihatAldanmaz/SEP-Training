cf0e4efce2d92170b64d3fe0353b398ef6a23715
tony <tonyz0212@gmail.com>
2020-09-16 22:05:56 -0700

    a

--- hw3/Promise.js
+++ hw3/Promise.js
@@ -85,3 +85,27 @@ new MyPromise((resolve, reject) => {
 //     }
 // ).catch(err => console.log('catchininginging'));
 
+
+function myfetch(url) {
+  return new Promise((res, rej) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const response = {
+          json: function () {
+            return JSON.parse(xhttp.responseText);
+          },
+        };
+        res(response);
+      }
+    };
+    xhttp.open('GET', url, true);
+    xhttp.send();
+  });
+}
+
+myfetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
