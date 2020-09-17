cbfb530232d7de0d26007913ca3212d168e3d2e6
Pu Du <pudugg@gmail.com>
2020-09-16 21:47:22 -0500

    implement promise.catch; still has bugs in promise.then

--- src/Promise/MyPromise.js
+++ src/Promise/MyPromise.js
@@ -1,6 +1,64 @@
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+
 class MyPromise {
   constructor(executor) {
-    executor();
+    this.status = PENDING;
+    this.value = null;
+    this.callbacks = [];
+    executor(this.resolve.bind(this), this.reject.bind(this));
+  }
+
+  resolve(value) {
+    if (this.status === PENDING) {
+      this.status = FULFILLED;
+      this.value = value;
+      setTimeout(() => {
+        this.callbacks.map((callback) => {
+          callback.onFulfilled(value);
+        });
+      });
+    }
+  }
+
+  reject(reason) {
+    if (this.status === PENDING) {
+      this.status = REJECTED;
+      this.value = reason;
+      setTimeout(() => {
+        this.callbacks.map((callback) => {
+          callback.onRejected(reason);
+        });
+      });
+    }
+  }
+
+  then(onFulfilled = () => {}, onRejected = () => {}) {
+    return new MyPromise((resolve, reject) => {
+      if (this.status === PENDING) {
+        this.callbacks.push({
+          onFulfilled,
+          onRejected,
+        });
+      }
+      if (this.status === FULFILLED) {
+        setTimeout(() => {
+          let result = onFulfilled(this.value);
+          resolve(result);
+        });
+      }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          let result = onRejected(this.value);
+          resolve(result);
+        });
+      }
+    });
+  }
+
+  catch(onRejected = () => {}) {
+    return this.then(undefined, onRejected);
   }
 
   static all(promises) {
@@ -28,16 +86,29 @@ class MyPromise {
   }
 }
 
-const asyncOperation = (time) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(time);
-    }, time);
+// const asyncOperation = (time) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(time);
+//     }, time);
+//   });
+// };
+
+// promises = [asyncOperation(1000), asyncOperation(2000), asyncOperation(3000)];
+
+// MyPromise.all(promises).then((results) => {
+//   console.log(results);
+// });
+
+new MyPromise((resolve, reject) => {
+  resolve('solved');
+})
+  .then((value) => {
+    console.log(value);
+    return 'success';
+  })
+  .then((value) => {
+    console.log(value);
   });
-};
 
-promises = [asyncOperation(1000), asyncOperation(2000), asyncOperation(3000)];
-
-MyPromise.all(promises).then((results) => {
-  console.log(results);
-});
+console.log('start');
