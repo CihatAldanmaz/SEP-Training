27cdb1c32c6aab6bc0df8302776992f3b3c6d229
ShuhaoBai <shuhao.bai@gmail.com>
2020-09-22 11:10:24 -0400

    update

--- HW6-iTunes/index.js
+++ HW6-iTunes/index.js
@@ -26,15 +26,32 @@ function start_search() {
         cors +
         `https://itunes.apple.com/search?term=${searchTerms}&media=music&entity=album&attribute=artistTerm&limit=50`;
 
-    fetch(URL)
-        .then(response => response.json())
-        .then(json =>
-            console.log(json)
-        )
+    handleResponse(URL)
 }
 
 
 
-function handleResponse() {
+function handleResponse(URL) {
+    fetch(URL)
+        .then(response => response.json())
+        // .then(json => console.log(json))
+        .then(json => renderPage(json))
+}
 
+function renderPage(json){
+    const{resultCount, results} = json;
+    let htmlString = "";
+    results.forEach(i => {
+        const albumName = i.collectionName;
+        const albumImgUrl = i.artworkUrl100;
+        htmlString += `<div class="album">
+        <img src="${albumImgUrl}" alt="Album Image">
+        <div class="album-name">${albumName}</div>
+        </div>`
+    });
+
+    const section = document.getElementsByTagName("section");
+    section.innerHTML = htmlString;
+    const resultTitle = document.querySelector(".result");
+    resultTitle.innerHTML = `${resultCount} Results for "${artisticName}"`;
 }
