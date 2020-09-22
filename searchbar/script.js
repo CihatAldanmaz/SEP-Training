f5e0939ea9a3287c89f7b40d00635e7611d514d5
echoliu123 <lulula19999@gmail.com>
2020-09-22 00:00:02 -0400

    searchbar css done

---
+++ searchbar/script.js
@@ -0,0 +1,8 @@
+ window.onload = function() {
+     var btn = document.querySelector(".btn");
+     var search = document.querySelector(".searcharea")
+
+     btn.onclick = function() {
+         alert(this.value)
+     }
+ }
