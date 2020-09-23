7ce0263d427600e618c589b680911997c249cf52
ShuhaoBai <shuhao.bai@gmail.com>
2020-09-22 23:06:25 -0400

    HW7 Setup webpack with sass loader, done

---
+++ HW7-webpack-loader-sass/src/index.js
@@ -0,0 +1,3 @@
+import './sass/styles.scss';
+
+console.log('HW7 to setup webpack sass loader')
---
+++ HW7-webpack-loader-sass/webpack.config.js
@@ -0,0 +1,56 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    // 1
+    entry: './src/index.js',
+    // 2
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        // This loader resolves url() and @imports inside CSS
+                        loader: "css-loader"
+                    },
+                    {
+                        // Then we apply postCSS fixes like autoprefixer and minifying
+                        loader: "postcss-loader"
+                    },
+                    {
+                        // First we transform SASS to standard CSS
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "bundle.css" })
+    ]
+
+};
