cb364feb33ae2a352916b63dcc24a4ae0884d7a8
show3567 <ydong011@fiu.edu>
2020-09-23 13:33:05 -0400

    update final for hw 01

--- React/React-basic/webpack.config.js
+++ React/React-basic/webpack.config.js
@@ -1,10 +1,30 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
-  mode: 'development',
-  entry: './lib/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
   },
+  
+  entry: "./lib/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'My App',
+    filename: './lib/index.html',
+    template: './src/index.html'
+  })],
 };
