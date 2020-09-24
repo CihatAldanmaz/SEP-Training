adddfc480fd16ba21e0eaa11ef81a6dc94f25728
Treagan Birbal <treaganbirbal@pursuit.org>
2020-09-24 13:26:27 -0400

    chore: create functionality to search itunes api, render data, reset form, rerender dom with new search

--- itunes_search/index.js
+++ itunes_search/index.js
@@ -1,37 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let ARTIST_NAME
-    const url = `https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=50`
+document.addEventListener("DOMContentLoaded", () => {
+  let ARTIST_NAME;
+  const url = `https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=50`;
 
-    const inputValue = document.querySelector('.artist_search-input')
-    const form  = document.querySelector('form')
-    const submit_Btn = document.querySelector('.artist_search-btn')
+  const inputValue = document.querySelector(".form_search-input");
+  const form = document.querySelector("form");
+  const submit_Btn = document.querySelector(".artist_search-btn");
 
-    const state = {
-        searchTerm :""
+  const state = {
+    searchTerm: "",
+    isSubmitted: false,
+  };
+  inputValue.addEventListener("change", (event) => {
+    state.searchTerm = event.target.value;
+  });
+
+  async function getArtist(name) {
+    return await fetch(
+      `https://itunes.apple.com/search?term=${name}&media=music&entity=album&attribute=artistTerm&limit=50`,
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    ).then((res) => res.json());
+  }
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    state.isSubmitted = true;
+
+    const div = document.createElement("div");
+    div.classList.add('artist-list')
+    getArtist(state.searchTerm).then((data) => {
+      data.results.forEach((el) => {
+        console.log(el);
+
+        const p = document.createElement("p");
+        const img = document.createElement("IMG");
+
+        img.src = el.artworkUrl100;
+        p.innerHTML = el.artistName;
+        document.querySelector("body").appendChild(div);
+        div.appendChild(p);
+        div.appendChild(img);
+      });
+    });
+    if(state.isSubmitted === true){
+    form.reset();
+    const artistList = document.querySelector('.artist-list')
+    document.querySelector('body').removeChild(artistList)
+    state.isSubmitted = false
     }
-    inputValue.addEventListener('change', (event) => {
-        state.searchTerm = event.target.value
-    })
-
-    async function getArtist(name){
-       return await fetch(`https://itunes.apple.com/search?term=${name}&media=music&entity=album&attribute=artistTerm&limit=50`, {
-           method: 'GET', 
-           mode: 'cors'
-       }).then(res => res.json())
-    }
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        getArtist(state.searchTerm).then(data => {
-            console.log(data.results[0])
-            data.results.forEach(el => {
-            const p = document.createElement('p')
-            const img = document.createElement('IMG')
-            img.src = el.artworkUrl100
-            p.innerHTML = el.artistName
-            document.querySelector('body').appendChild(p)
-            document.querySelector('body').appendChild(img)
-            })
-        })
-
-    })
-})
+  });
+});
