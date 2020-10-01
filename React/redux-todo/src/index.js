7488f8cd9f17987c6c31a10aa7674875de2793a6
Geethika Rajanala <geethu@Geethikas-Air.attlocal.net>
2020-10-01 12:40:38 -0400

    redux to-do almost done

--- React/redux-todo/src/index.js
+++ React/redux-todo/src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
+import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducers/reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = () => {
+    ReactDOM.render(
+        <Provider store={store}><App /></Provider>, document.getElementById('root'));
+};
+
+store.subscribe(render);
+render();
+
+registerServiceWorker();
